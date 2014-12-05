95335ffd0b8243f521e3be975320c8be2be14494
unknown <laufengd@EAS-BEN016D2.it.muohio.edu>
2014-12-04 21:26:34 -0500

    Fixed bubbleUp function and added more test cases

--- Heap.h
+++ Heap.h
@@ -46,4 +46,126 @@ private:
   void trickleDown(unsigned long index);  
 };
 
-#include "Heap.ipp"
+#include <string>
+
+template<class Pri, class T>
+Heap<Pri,T>::Heap(){
+	numItems = 0;
+	arrSize = START_SIZE;
+	backingArray = new std::pair<Pri, T>[arrSize];
+}
+
+template<class Pri, class T>
+Heap<Pri,T>::~Heap(){
+	delete[] backingArray;
+	backingArray = NULL;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::grow(){
+	unsigned int newSize = arrSize*2;
+	std::pair<Pri, T>* myNewArray = new std::pair<Pri, T>[newSize];
+
+	//Copy over the old items
+	for(int i=0; i < arrSize; i++){
+		myNewArray[i] = backingArray[i];
+}	
+	delete[] backingArray;
+	backingArray = myNewArray;
+	arrSize = newSize;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::add(std::pair<Pri,T> toAdd){
+	if(numItems+1 > arrSize) grow();
+	backingArray[numItems] = toAdd;
+	bubbleUp(numItems);
+
+	numItems++;
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::bubbleUp(unsigned long index){
+	int p = (index-1)/2; // gives the index for parent
+	
+	while(index > 0 && backingArray[index] < backingArray[p]){
+		
+		//switches parent and child pair if the parent is greater than the child
+		std::pair<Pri, T> temp = backingArray[p];
+		backingArray[p] = backingArray[index];
+		backingArray[index] = temp;
+		index = (index-1)/2;
+		p = (index-1)/2;
+	}
+	
+}
+
+template<class Pri, class T>
+void Heap<Pri,T>::trickleDown(unsigned long index){
+	bool s;
+	do{
+		int j = -1;
+		s = false; //tells whether or not a value has been swapped
+		int r = 2*index+2;
+		
+		if( r < numItems && backingArray[r] < backingArray[index]){  //check right to see if right child is less than parent
+			
+			int l = (2*index)+1;
+				
+			if(backingArray[l] < backingArray[r]){ //check if left child is less than right child and assigns j = to the value of the lesser one
+				j = l;
+				s = true;
+			}else{
+				j = r;
+				s = true;
+			}
+		
+		
+		
+		}else{
+			int l = (2*index)+1;
+			if(l < numItems && backingArray[l] < backingArray[index]){
+				j = l;
+				s = true;
+			}
+		}
+
+		
+		if(j >= 0){
+			
+			//swaps the pairs of indexes j and 'index'
+			std::pair<Pri, T> temp = backingArray[index];
+			backingArray[index] = backingArray[j];
+			backingArray[j] = temp;
+		}
+
+		index = j;
+	
+	} while (s == true); // if no swap occurs then function is finished
+	
+}
+
+template<class Pri, class T>
+std::pair<Pri,T> Heap<Pri,T>::remove(){
+  
+	
+	
+  std::pair<Pri,T> tmp = backingArray[0];
+
+  if(numItems == 0){
+	throw std::string("no more items to remove");
+  
+  }
+
+  backingArray[0] = backingArray[numItems-1];
+  numItems--;
+  trickleDown(0);
+  //numItems--;
+  return tmp;
+}
+
+template<class Pri, class T>
+unsigned long Heap<Pri,T>::getNumItems(){
+	return numItems;
+}
+
--- main.cpp
+++ main.cpp
@@ -49,6 +49,39 @@ void testHeap(){
     std::cout << "SUCCESS: Found item 5 as expected." << std::endl;
   }
 
+  myHeap.add(std::make_pair(3,"3"));
+  myHeap.add(std::make_pair(-99,"-99"));
+  myHeap.add(std::make_pair(199,"199"));
+  myHeap.add(std::make_pair(1000,"1000"));
+  myHeap.add(std::make_pair(-1,"-1"));
+  myHeap.add(std::make_pair(150,"150"));
+
+
+
+  item = myHeap.remove();
+  if(item.first != -99){
+    std::cout << "ERROR: Expected key -99, but got " << item.first << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Found item -99 as expected." << std::endl;
+  }
+
+  item = myHeap.remove();
+  if(item.first != -1){
+    std::cout << "ERROR: Expected key -1, but got " << item.first << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Found item -1 as expected." << std::endl;
+  }
+
+  item = myHeap.remove();
+  if(item.first != 3){
+    std::cout << "ERROR: Expected key 3, but got " << item.first << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Found item 3 as expected." << std::endl;
+  }
+
   item = myHeap.remove();
   if(item.first != 7){
     std::cout << "ERROR: Expected key 7, but got " << item.first << std::endl;
@@ -57,7 +90,7 @@ void testHeap(){
     std::cout << "SUCCESS: Found item 7 as expected." << std::endl;
   }
 
-  item = myHeap.remove();
+   item = myHeap.remove();
   if(item.first != 10){
     std::cout << "ERROR: Expected key 10, but got " << item.first << std::endl;
     exit(1);
@@ -65,6 +98,28 @@ void testHeap(){
     std::cout << "SUCCESS: Found item 10 as expected." << std::endl;
   }
 
+  item = myHeap.remove();
+  if(item.first != 150){
+    std::cout << "ERROR: Expected key 150, but got " << item.first << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Found item 150 as expected." << std::endl;
+  }
+
+   item = myHeap.remove();
+  if(item.first != 199){
+    std::cout << "ERROR: Expected key 199, but got " << item.first << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Found item 199 as expected." << std::endl;
+  }
+   item = myHeap.remove();
+  if(item.first != 1000){
+    std::cout << "ERROR: Expected key 1000, but got 1000" << item.first << std::endl;
+    exit(1);
+  } else {
+    std::cout << "SUCCESS: Found item 1000 as expected." << std::endl;
+  }
 }
 
 int main(){
